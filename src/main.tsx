2774a7b94cb803ecd43c4f38c5c68adbe16ebd67
victor <quesvictor4235@gmail.com>
2026-08-23 23:24:21 +0200

    lobby works on same browser

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import { Repo } from "@automerge/automerge-repo"
+import { BrowserWebSocketClientAdapter } from "@automerge/automerge-repo-network-websocket"
 import { BroadcastChannelNetworkAdapter } from "@automerge/automerge-repo-network-broadcastchannel"
 import { IndexedDBStorageAdapter } from "@automerge/automerge-repo-storage-indexeddb"
 import { RepoContext } from "@automerge/automerge-repo-react-hooks"
@@ -8,15 +9,18 @@ import App from "./App.tsx"
 import "./index.css"
 import { getOrCreateRoot } from "./rootDoc"
 
+const wsAdapter = new BrowserWebSocketClientAdapter("wss://sync.automerge.org")
+
 export const repo = new Repo({
   storage: new IndexedDBStorageAdapter("automerge-final"),
   network: [
+    wsAdapter,
     new BroadcastChannelNetworkAdapter(),
   ],
 })
 
-export const goOffline = () => { console.log("Offline") }
-export const goOnline = () => { console.log("Online") }
+export const goOffline = () => { wsAdapter.disconnect() }
+export const goOnline = () => { wsAdapter.connect(repo.peerId) }
 
 const rootDocUrl = getOrCreateRoot(repo)
 
